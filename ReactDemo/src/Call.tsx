170d72886c49739c758829fdd7b249b60b948bc2
GZY <gzy@GZYdeMacBook-Pro.local>
2020-07-03 18:25:26 +0800

    demo: refact

--- ReactDemo/src/Call.tsx
+++ ReactDemo/src/Call.tsx
@@ -1,88 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import agoraManager from './agora';
-import useRemoteUsers from './hooks/useRemoteUsers';
+import React, { useState } from 'react';
+import AgoraRTC from 'agora-rtc-sdk-ng';
+import useAgora from './hooks/useAgora';
+import MediaPlayer from './components/MediaPlayer';
 import './Call.css';
 
+const client = AgoraRTC.createClient({ codec: 'h264', mode: 'rtc' });
+
 function Call() {
-  const [ appid, setAppid ] = useState("");
-  const [ token, setToken ] = useState("");
-  const [ channel, setChannel ] = useState("");
-  const [ localPlayerText, setLocalPlayerText ] = useState("");
-  const [ client, setClient ] = useState(agoraManager.client);
-  const [remoteUsers, newUser, setRemoteUsers] = useRemoteUsers(client);
-  const [ joinState, setJoinState ] = useState(false);
-
-  // when the new user publishs tracks, play them.
-  useEffect(() => {
-    (async () => {
-      if (!newUser || !client) return;
-      await client.subscribe(newUser);
-      newUser.videoTrack?.play(`remote-player-${newUser.uid}`);
-      newUser.audioTrack?.play();
-    })();
-  }, [newUser]);
-
-  // init the client after this component is mounted.
-  useEffect(() => {
-    agoraManager.createClient({ codec: 'h264', mode: 'rtc' });
-    setClient(agoraManager.client);
-  }, []);
-
-
-  async function joinChannel() {
-    const client = agoraManager.client;
-    if (!client) return;
-    const [microphoneTrack, cameraTrack] = await agoraManager.createLocalTracks();
-    cameraTrack.play('local-player');
-    setLocalPlayerText(`localVideo`);
-    
-    await client.join(appid, channel, token || null);
-    setLocalPlayerText(`localVideo(${client.uid})`);
-    setJoinState(true);
-
-    await client.publish([microphoneTrack, cameraTrack]);
-  }
-
-  async function leaveChannel() {
-    setRemoteUsers([]);
-    setLocalPlayerText('');
-    agoraManager.localTracks.cameraTrack?.stop();
-    agoraManager.localTracks.microphoneTrack?.stop();
-    await client?.leave();
-    setJoinState(false);
-  }
+  const [ appid, setAppid ] = useState('');
+  const [ token, setToken ] = useState('');
+  const [ channel, setChannel ] = useState('');
+  const {
+    localAudioTrack, localVideoTrack, leave, join, joinState, remoteUsers
+  } = useAgora(client);
 
   return (
-    <div className="call">
-      <form className="call-form">
+    <div className='call'>
+      <form className='call-form'>
         <label>
           AppID:
-          <input type="text" name="appid" onChange={(event) => { setAppid(event.target.value) }}/>
+          <input type='text' name='appid' onChange={(event) => { setAppid(event.target.value) }}/>
         </label>
         <label>
           Token(Optional):
-          <input type="text" name="token" onChange={(event) => { setToken(event.target.value) }} />
+          <input type='text' name='token' onChange={(event) => { setToken(event.target.value) }} />
         </label>
         <label>
           Channel:
-          <input type="text" name="channel" onChange={(event) => { setChannel(event.target.value) }} />
+          <input type='text' name='channel' onChange={(event) => { setChannel(event.target.value) }} />
         </label>
-        <div className="button-group">
-          <button id="join" type="button" className="btn btn-primary btn-sm" disabled={joinState} onClick={joinChannel}>Join</button>
-          <button id="leave" type="button" className="btn btn-primary btn-sm" disabled={!joinState} onClick={leaveChannel}>Leave</button>
+        <div className='button-group'>
+          <button id='join' type='button' className='btn btn-primary btn-sm' disabled={joinState} onClick={() => {join(appid, channel, token)}}>Join</button>
+          <button id='leave' type='button' className='btn btn-primary btn-sm' disabled={!joinState} onClick={() => {leave()}}>Leave</button>
         </div>
       </form>
-      <div className="player-container">
-        <div className="local-player-wrapper">
-          <p className="local-player-text">{localPlayerText}</p>
-          <div id="local-player" style={{width: '360px', height: '240px'}}></div>
+      <div className='player-container'>
+        <div className='local-player-wrapper'>
+          <p className='local-player-text'>{localVideoTrack && `localTrack`}{joinState && localVideoTrack ? `(${localVideoTrack.getUserId()})` : ''}</p>
+          <MediaPlayer videoTrack={localVideoTrack} audioTrack={localAudioTrack}></MediaPlayer>
         </div>
-        {remoteUsers.map(user => (
-          <div className="remote-player-wrapper" key={user.uid}>
-            <p className="remote-player-text">{`remoteVideo(${user.uid})`}</p>
-            <div id={`remote-player-${user.uid}`} style={{width: '360px', height: '240px'}}></div>
-          </div>
-        ))}
+        {remoteUsers.map(user => (<div className='remote-player-wrapper' key={user.uid}>
+            <p className='remote-player-text'>{`remoteVideo(${user.uid})`}</p>
+            <MediaPlayer videoTrack={user.videoTrack} audioTrack={user.audioTrack}></MediaPlayer>
+          </div>))}
       </div>
     </div>
   );
